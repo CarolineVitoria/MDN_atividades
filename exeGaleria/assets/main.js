c75a0ad2948390c073e3443ed8a28a38d7238ab7
caroline vitória <carolmelo007@hotmail.com>
2022-08-11 09:04:15 -0300

    utilizando a delegação de eventos

--- exeGaleria/assets/main.js
+++ exeGaleria/assets/main.js
@@ -1,19 +1,25 @@
 function meuEscopo(){
     const img_full = document.querySelector('.displayed_img');
-    let imgs = document.querySelectorAll('.a');
+    //let imgs = document.querySelectorAll('.a');
     const bt = document.querySelector('.link');
     const d_dark = document.querySelector('.d_dark');
     const svgOn= document.querySelector('#svg_on');
     const svgOff= document.querySelector('#svg_off');
     let attSrc;
     let contador=0;
+    const overlay = document.querySelector('.overlay');
 
-    for(let i =0; i<imgs.length; i++){
-        imgs[i].addEventListener('click', function(e){
-            attSrc=e.target.getAttribute('src').replace('/175/175', '/900/500');
-            img_full.setAttribute('src', attSrc);
-        })
-    }
+    // for(let i =0; i<imgs.length; i++){
+    //     imgs[i].addEventListener('click', function(e){
+    //         attSrc=e.target.getAttribute('src').replace('/175/175', '/900/500');
+    //         img_full.setAttribute('src', attSrc);
+    //     })
+    // }
+    //utilizando a delegação de eventos
+    overlay.addEventListener('click', function(e){
+        attSrc=e.target.getAttribute('src').replace('/175/175', '/900/500');
+        img_full.setAttribute('src', attSrc);
+    })
     bt.addEventListener('click', function(){
         contador+=1;
         if(contador%2 !== 0){
