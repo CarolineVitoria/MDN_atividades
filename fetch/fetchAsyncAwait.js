237cb1d0d4d0ecde3080b2944fc26f869e190d28
Caroline Vitóra <carolmelo007@hotmail.com>
2022-09-21 14:30:21 -0300

    add tratamento de erros

--- fetch/fetchAsyncAwait.js
+++ fetch/fetchAsyncAwait.js
@@ -1,7 +1,11 @@
-async function retornaJson(){
-    let promise= await fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits');
-    let response = await promise.json();
-    console.log(response[0].author.login);
+async function f(){
+    try{
+        let promise= await fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits');
+        let response = await promise.json();
+        console.log(response[0].author.login);  
+    }catch(e){
+        console.log(e);
+    }
 }
-retornaJson();
+f();
 
